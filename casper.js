2e71c43d4c363dce80bcac698a1007286cd3314f
jpca999 <jpca999@gmail.com>
2017-02-22 03:38:48 -0500

    first commit

---
+++ casper.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+
+var casper = require('casper').create();
+
+casper.start('http://casperjs.org/', function() {
+    this.echo(this.getTitle());
+});
+
+casper.thenOpen('http://phantomjs.org', function() {
+    this.echo(this.getTitle());
+});
+
+casper.run();
